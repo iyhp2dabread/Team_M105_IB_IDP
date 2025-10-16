3b01c165e260df3899ba80b4489a1ecf108c4a5a
DROP DATABASE <112593979+iyhp2dabread@users.noreply.github.com>
2025-10-16 09:43:26 +0100

    Update red_led.py

--- red_led.py
+++ red_led.py
@@ -1,7 +1,7 @@
 from machine import Pin
 from utime import sleep
 
-def red_led(): #turns Red LED on
+def red_led(): #turns red LED on
     #amber LED
     led_amber_pin = 28  # Pin 28 = GP28 (labelled 34 on the jumper)
     led_amber = Pin(led_amber_pin, Pin.OUT)
@@ -11,11 +11,12 @@ def red_led(): #turns Red LED on
     led_red = Pin(led_red_pin, Pin.OUT)
 
     print("Red LED ON")
-    led_red.value(1)
     led_amber.value(0)
-    sleep(5)
-    led_red.value(0)
-    print("test complete")
+    while True:
+        led_red.value(1)
+        sleep(0.5)
+        led_red.value(0)
+        sleep(0.5)
 
 if __name__ == "__main__":
     red_led()
