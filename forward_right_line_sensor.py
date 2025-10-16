2b07ad5692c0918cd6a41d06d1e9c089daccec7d
DROP DATABASE <112593979+iyhp2dabread@users.noreply.github.com>
2025-10-16 10:49:28 +0100

    Create forward_right_line_sensor.py

---
+++ forward_right_line_sensor.py
@@ -0,0 +1,13 @@
+#line sensor detection for robot
+from machine import Pin
+from utime import sleep
+
+#Set the line_sensor pin for forward right
+sensor_pin = 21 # Pin 21 = GP21 (labelled 27 on the jumper)
+sensor_forward_right = Pin(sensor_pin, Pin.IN, Pin.PULL_DOWN)
+
+#Continiously update the sensor value and print said value
+while True:
+  sleep(0.1)
+  print(sensor_forward_right.value())
+  return sensor_forward_right.value()
