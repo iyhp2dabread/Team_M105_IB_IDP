05377d891088794151f951983ebac1164ea8892b
DROP DATABASE <112593979+iyhp2dabread@users.noreply.github.com>
2025-10-17 11:13:19 +0100

    Update forward_right_line_sensor.py

--- forward_right_line_sensor.py
+++ forward_right_line_sensor.py
@@ -7,7 +7,12 @@ sensor_pin = 21 # Pin 21 = GP21 (labelled 27 on the jumper)
 sensor_forward_right = Pin(sensor_pin, Pin.IN, Pin.PULL_DOWN)
 
 #Continiously update the sensor value and print said value
-while True:
-  sleep(0.1)
-  print(sensor_forward_right.value())
-  return sensor_forward_right.value()
+def forward_right_sensor():
+    print("Sequence Forward Right Active")
+    while True:
+      sleep(0.1)
+      print(sensor_forward_right.value())
+      return sensor_forward_right.value()
+
+if __name__ == "__main__":
+    forward_right_sensor()
