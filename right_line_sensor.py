e30a2f7d0e0228a8459194000ea31bfdc5625d5c
DROP DATABASE <112593979+iyhp2dabread@users.noreply.github.com>
2025-10-17 11:09:40 +0100

    Update right_line_sensor.py

--- right_line_sensor.py
+++ right_line_sensor.py
@@ -4,10 +4,15 @@ from utime import sleep
 
 #Set the line_sensor pin for right
 sensor_pin = 11 # Pin 11 = GP11 (labelled 15 on the jumper)
-sensor_right = Pin(sensor_pin, Pin.IN, Pin.PULL_DOWN)
+sensor_left = Pin(sensor_pin, Pin.IN, Pin.PULL_DOWN)
 
 #Continiously update the sensor value and print said value
-while True:
-  sleep(0.1)
-  print(sensor_right.value())
-  return sensor_right.value()
+def right_sensor():
+    print("Sequence Right Active")
+    while True:
+      sleep(0.1)
+      print(sensor_right.value())
+      return sensor_right.value()
+
+if __name__ == "__main__":
+    right_sensor()
