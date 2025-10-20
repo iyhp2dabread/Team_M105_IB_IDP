f276471bd6f1924f77f57fcf36b46629190dfc12
DROP DATABASE <112593979+iyhp2dabread@users.noreply.github.com>
2025-10-20 12:21:55 +0100

    Update forward_left_line_sensor.py

--- Sensors/forward_left_line_sensor.py
+++ Sensors/forward_left_line_sensor.py
@@ -3,7 +3,7 @@ from machine import Pin
 from utime import sleep
 
 #Set the line_sensor pin for forward left
-sensor_pin = 20 # Pin 20 = GP20 (labelled 24 on the jumper)
+sensor_pin = 11 # Pin 11 = GP11 (labelled 15 on the jumper)
 sensor_forward_left = Pin(sensor_pin, Pin.IN, Pin.PULL_DOWN)
 
 #Continiously update the sensor value and print said value
