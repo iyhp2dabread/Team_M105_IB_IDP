2012cbaad18651e38495f2936b067a0ec5aa3a2f
DROP DATABASE <112593979+iyhp2dabread@users.noreply.github.com>
2025-10-21 21:03:09 +0100

    Create area1_to_bay.py

---
+++ Area/area1_to_bay.py
@@ -0,0 +1,38 @@
+from machine import Pin, PWM
+from utime import sleep
+
+from turn_left import turn_left
+from turn_right import turn_right
+from left_line_sensor import left_sensor
+from right_line_sensor import right_sensor
+from lower_junction_to_bay_entrance import lower_junction_to_bay_entrance
+
+from placement_right.py import place_right
+
+def area1_to_bay():
+    left_sense = left_sensor()
+    right_sense = right_sensor()
+    
+    turn_right(1)
+    while left_sense != 1 and right_sense != 1:
+        go_forward(0.1)
+        left_sense = left_sensor()
+        right_sense = right_sensor()
+    #on the checkpoint in area 1
+    
+    go_forward(0.2)
+    left_sense = 0
+    while left_sense != 1:
+        go_forward(0.2)
+        left_sense = left_sensor()
+    #on the top right corner
+        
+    turn_left(1)
+    go_forward(0.2)
+    left_sense = 0
+    while left_sense != 1:
+        go_forward(0.2)
+        left_sense = left_sensor()
+    #on lower junction
+    
+    lower_junction_to_bay_entrance()
