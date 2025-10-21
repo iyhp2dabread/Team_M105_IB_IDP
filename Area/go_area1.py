0de15e1e1233f0554d815e525f4a642d45584571
DROP DATABASE <112593979+iyhp2dabread@users.noreply.github.com>
2025-10-21 20:01:46 +0100

    Update go_area1.py

--- Area/go_area1.py
+++ Area/go_area1.py
@@ -8,12 +8,12 @@ from right_line_sensor import right_sensor
 
 from placement_right.py import place_right
 
-def go_to_area1():
+def go_to_area1(index):
     left_sense = left_sensor()
     right_sense = right_sensor()
     while left_sense != 1 and right_sense != 1:
         go_forward(0.1)
         left_sense = left_sensor()
         right_sense = right_sensor()
-    place_right()
+    place_right(index)
     
