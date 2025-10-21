a00d2f9d8bb292d45cba16ef5aff71c41754bdb7
DROP DATABASE <112593979+iyhp2dabread@users.noreply.github.com>
2025-10-21 21:12:37 +0100

    Create area2_to_lower_junction.py

---
+++ Area/area2_to_lower_junction.py
@@ -0,0 +1,41 @@
+from machine import Pin, PWM
+from utime import sleep
+
+from turn_left import turn_left
+from turn_right import turn_right
+from left_line_sensor import left_sensor
+from right_line_sensor import right_sensor
+from lower_junction_to_bay_entrance import lower_junction_to_bay_entrance
+
+def area2_to_lj():
+    left_sense = left_sensor()
+    right_sense = right_sensor()
+
+    turn_right(1)
+    right_sense = 0
+    while right_sense != 1:
+        go_forward(0.1)
+        right_sense = right_sensor()
+    #on the middle right corner
+    
+    turn_right(1)
+    go_forward(0.2)
+    right_sense = 0
+    while right_sense != 1:
+        go_forward(0.1)
+        right_sense = right_sensor()
+    #on the upper junction
+        
+    turn_right(1)
+    go_forward(0.2)
+    left_sense = 0
+    right_sense = 0
+    while left_sense != 1 and right_sense != 1:
+        go_forward(0.2)
+        left_sense = left_sensor()
+        right_sense = right_sensor()
+    #on lower junction
+    turn_left(1)
+    
+    lower_junction_to_bay_entrance() 
+
