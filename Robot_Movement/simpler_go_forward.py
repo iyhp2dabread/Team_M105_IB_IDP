b3de6ae2bcc0ab90c1ca2e8ec89d35ccdc843810
R-chi46 <akc46@cam.ac.uk>
2025-10-22 09:19:01 +0100

    Implement motor control for forward movement

---
+++ Robot_Movement/simpler_go_forward.py
@@ -0,0 +1,61 @@
+
+#Goes forward, using motors.
+from machine import Pin, PWM
+from utime import sleep
+#from refind_line import refind_line
+from forward_right_line_sensor import forward_right_sensor
+from forward_left_line_sensor import forward_left_sensor
+from left_line_sensor import left_sensor
+from right_line_sensor import right_sensor
+
+class Motor:
+    def __init__(self, dirPin, PWMPin):
+        self.mDir = Pin(dirPin, Pin.OUT)  # set motor direction pin
+        self.pwm = PWM(Pin(PWMPin))  # set motor pwm pin
+        self.pwm.freq(1000)  # set PWM frequency
+        self.pwm.duty_u16(0)  # set duty cycle - 0=off
+        
+    def off(self):
+        self.pwm.duty_u16(0)
+        
+    def Forward(self, speed=100):
+        self.mDir.value(0)                     # forward = 0 reverse = 1 motor
+        self.pwm.duty_u16(int(65535 * speed / 100))  # speed range 0-100 motor
+
+    def Reverse(self, speed=30):
+        self.mDir.value(1)
+        self.pwm.duty_u16(int(65535 * speed / 100))
+
+
+            
+def go_forward(t):
+
+    motor3 = Motor(dirPin=4, PWMPin=5)  # Motor 3 is controlled from Motor Driv2 #1, which is on GP4/5
+    motor4 = Motor(dirPin=7, PWMPin=6)  # Motor 4 is controlled from Motor Driv2 #2, which is on GP6/7
+    
+    print("Go forward")
+    if forward_left_sensor() == 0 and forward_right_sensor() == 1:
+        motor3.off()
+        motor4.Forward(t)
+        sleep(t)# Puts power through right motor only to make small correction
+        motor4.off()
+    if forward_left_sensor() == 1 and forward_right_sensor() == 0:
+        motor4.off()
+        motor3.Forward(t) # Puts power through left motor only to make small correction
+        sleep(t)
+        motor3.off()
+    if forward_left_sensor() == 1 and forward_right_sensor() == 0:
+        motor3.Forward(1)
+        motor4.Forward(1)
+        sleep(t)
+                
+    
+    
+           # else:
+           #     motor3.off()
+           #     motor4.off()
+            #    refind_line()     # Robot has come of line completely and must refind it 
+
+if __name__ == "__main__":
+    go_forward(1)
+        
