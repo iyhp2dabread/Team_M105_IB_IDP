58d46223bf0fc3908d8a58d1bdf3b9776c517f71
DROP DATABASE <112593979+iyhp2dabread@users.noreply.github.com>
2025-10-23 09:20:25 +0100

    Update auto_correct.py

--- Robot_Movement/auto_correct.py
+++ Robot_Movement/auto_correct.py
@@ -6,14 +6,15 @@ from forward_left_line_sensor import forward_left_sensor
 from forward_right_line_sensor import forward_right_sensor
 from turn_left import turn_left
 
-fl_sense = forward_left_sensor()
-fr_sense = forward_right_sensor()
-
-print("Auto Correct Start")
-
-while fl_sense != 1 and fr_sense != 1:
-    turn_left(0.1)
+def auto_correct():
     fl_sense = forward_left_sensor()
     fr_sense = forward_right_sensor()
 
-print("Auto Correct End")
+    print("Auto Correct Start")
+
+    while fl_sense != 1 and fr_sense != 1:
+        turn_left(0.1)
+        fl_sense = forward_left_sensor()
+        fr_sense = forward_right_sensor()
+
+    print("Auto Correct End")
