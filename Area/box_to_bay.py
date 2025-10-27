d13f293fcf454207380dbd4bf22d88148c654387
DROP DATABASE <112593979+iyhp2dabread@users.noreply.github.com>
2025-10-27 12:44:22 +0000

    Create box_to_bay.py

---
+++ Area/box_to_bay.py
@@ -0,0 +1,31 @@
+#box to bay exit
+from stop import stop
+from turn_left import turn_left
+from turn_right import turn_right
+from forward_left_line_sensor import forward_left_sensor
+from forward_right_line_sensor import forward_right_sensor
+from machine import Pin, PWM
+from utime import sleep
+from left_line_sensor import left_sensor
+from right_line_sensor import right_sensor
+from go_forward_continuous import go_forward_continuous
+
+print("Welcome to main.py!")
+
+def box_to_bay():
+    #assuming we are on the start box right side facing east
+    turn_left(1.1)
+    go_forward_continous(2)
+    turn_left(1.1)
+    go_forward_continous(1)
+    turn_right(1.1)
+    go_forward_continous(2)
+    #we are now out of the box and on the bay line
+    
+    turn_left(1)
+    go_forward(5)
+    go_forward(5)
+    #Now at bay entrance
+    
+    turn_right(2.2)
+    #should be facing east
