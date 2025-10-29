3ed1a7a8716e30e27280cc9171ec36b76994c9b8
R-chi46 <akc46@cam.ac.uk>
2025-10-29 09:15:39 +0000

    Implement linear actuator control in Python

---
+++ Scrapyard/linear_actuator_lowering.py
@@ -0,0 +1,27 @@
+from machine import Pin, PWM
+from utime import sleep
+
+class Actuator:
+    def __init__(self, dirPin, PWMPin):
+        self.mDir = Pin(dirPin, Pin.OUT)  # set motor direction pin
+        self.pwm = PWM(Pin(PWMPin))  # set motor pwm pin
+        self.pwm.freq(1000)  # set PWM frequency
+        self.pwm.duty_u16(0)  # set duty cycle - 0=off
+           
+    def set(self, dir, speed):
+        self.mDir.value(dir)                     # forward = 0 reverse = 1 motor
+        self.pwm.duty_u16(int(65535 * speed / 100))  # speed range 0-100 motor
+
+
+def lift_sequence():
+    actuator1 = Actuator(dirPin=0, PWMPin=1)  # Actuator 1 controlled from Motor Driv1 #1, which is on GP0/1
+    print("Extending quickly")
+    actuator1.set(dir = 1, speed = 50)
+    sleep(8)
+    actuator1.set(dir = 0, speed=50)
+    sleep(4.5)  # nb we don't know when this has finished without another means
+    actuator1.set(dir = 0, speed = 0)
+    #print("Retracing slowly")
+    #actuator1.set(dir=1, speed=25)
+    #sleep(20)  # nb we don't know when this has finished without another means
+    
