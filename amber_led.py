db6a96bb706c5070daa0bd7c6d9d4cb2aa3e87ce
sf790 <sf790@cam.ac.uk>
2025-10-30 10:44:32 +0000

    Rename amber_led function and add off function

--- amber_led.py
+++ amber_led.py
@@ -1,18 +1,23 @@
 from machine import Pin
 from utime import sleep
 
-def amber_led(): #turns amber LED on
+def amber_led_on(): #turns amber LED on
     #amber LED
     led_amber_pin = 28  # Pin 28 = GP28 (labelled 34 on the jumper)
     led_amber = Pin(led_amber_pin, Pin.OUT)
-    
-    #Red LED
-    led_red_pin = 8  # Pin 8 = GP8 (labelled 11 on the jumper)
-    led_red = Pin(led_red_pin, Pin.OUT)
 
     print("Amber LED ON")
-    led_red.value(0)
     led_amber.value(1)
 
+def amber_led_off(): #turns amber LED off
+    #amber LED
+    led_amber_pin = 28  # Pin 28 = GP28 (labelled 34 on the jumper)
+    led_amber = Pin(led_amber_pin, Pin.OUT)
+
+    print("Amber LED OFF")
+    led_amber.value(0)
+
 if __name__ == "__main__":
-    amber_led()
+    amber_led_on()
+    sleep(5)
+    amber_led_off()
