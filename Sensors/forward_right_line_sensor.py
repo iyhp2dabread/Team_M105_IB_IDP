b89674407b11518dcb01b59c588657eb60b38c73
DROP DATABASE <112593979+iyhp2dabread@users.noreply.github.com>
2025-11-04 09:41:16 +0000

    Update forward_right_line_sensor.py

--- Sensors/forward_right_line_sensor.py
+++ Sensors/forward_right_line_sensor.py
@@ -3,14 +3,15 @@ from machine import Pin
 from utime import sleep
 
 #Set the line_sensor pin for forward right
-sensor_pin = 20 # Pin 20 = GP20 (labelled 26 on the jumper)
+sensor_pin = 20 # Pin 21 = GP21 (labelled 27 on the jumper)
 sensor_forward_right = Pin(sensor_pin, Pin.IN, Pin.PULL_DOWN)
 
 #Continiously update the sensor value and print said value
 def forward_right_sensor():
-    print("Sequence Forward Right Active")
+    #print("Sequence Forward Right Active")
     while True:
       sleep(0.01)
+      print(sensor_forward_right.value())
       return sensor_forward_right.value()
 
 if __name__ == "__main__":
