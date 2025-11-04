58aa33498fbea0eb2444ba8c2484eb185e9dabfe
DROP DATABASE <112593979+iyhp2dabread@users.noreply.github.com>
2025-11-04 09:39:32 +0000

    Update distance_sensor.py

--- Sensors/distance_sensor.py
+++ Sensors/distance_sensor.py
@@ -4,8 +4,8 @@ from utime import sleep
 
 def distance_sensor():
      # config I2C Bus
-    i2c_bus = I2C(id=0, sda=Pin(8), scl=Pin(9)) # I2C0 on GP8 & GP9
-    # print(i2c_bus.scan())  # Get the address (nb 41=0x29, 82=0x52)
+    i2c_bus = I2C(id=1, sda=Pin(18), scl=Pin(19)) # I2C1 on GP18 & GP19
+    print(i2c_bus.scan())  # Get the address (nb 41=0x29, 82=0x52)
     
     # Setup vl53l0 object
     vl53l0 = VL53L0X(i2c_bus)
@@ -13,9 +13,20 @@ def distance_sensor():
     vl53l0.set_Vcsel_pulse_period(vl53l0.vcsel_period_type[1], 14)
 
     # Start device
-    vl53l0.start()
-    distance = vl53l0.read()
-    print("Distance = {distance}mm")
-    vl53.l0.stop()
-    return distance 
+    while True:
+        print("Starting vl53l0...")
 
+        # Start device
+        vl53l0.start()
+
+        # Read ten samples
+        for _ in range(10):
+            distance = vl53l0.read()
+            print(f"Distance = {distance}mm")  # Check calibration!
+            sleep(0.5)
+        
+        # Stop device
+        vl53l0.stop()
+        
+if __name__ == "__main__":
+    distance_sensor()
