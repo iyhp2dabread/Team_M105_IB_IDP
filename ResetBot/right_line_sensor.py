d2b204c86465d181ccba6d7d4d0c8846c6e72495
DROP DATABASE <112593979+iyhp2dabread@users.noreply.github.com>
2025-11-05 09:23:54 +0000

    Create right_line_sensor.py

---
+++ ResetBot/right_line_sensor.py
@@ -0,0 +1,18 @@
+#line sensor detection for robot
+from machine import Pin
+from utime import sleep
+
+#Set the line_sensor pin for right
+sensor_pin = 21 # Pin 21 = GP21 (labelled 27 on the jumper)
+sensor_right = Pin(sensor_pin, Pin.IN, Pin.PULL_DOWN)
+
+#Continiously update the sensor value and print said value
+def right_sensor():
+    #print("Sequence Right Active")
+    while True:
+      sleep(0.01)
+      print(sensor_right.value())
+      return sensor_right.value()
+
+if __name__ == "__main__":
+    right_sensor()
