c5d66366591cc27f6375a8c6572a163ddfa0ae1c
DROP DATABASE <112593979+iyhp2dabread@users.noreply.github.com>
2025-11-05 09:20:14 +0000

    Create forward_left_line_sensor.py

---
+++ ResetBot/forward_left_line_sensor.py
@@ -0,0 +1,18 @@
+#line sensor detection for robot
+from machine import Pin
+from utime import sleep
+
+#Set the line_sensor pin for forward left
+sensor_pin = 11 # Pin 11 = GP11 (labelled 15 on the jumper)
+sensor_forward_left = Pin(sensor_pin, Pin.IN, Pin.PULL_DOWN)
+
+#Continiously update the sensor value and print said value
+def forward_left_sensor():
+ #   print("Sequence Forward Left Active")
+    while True:
+      sleep(0.01)
+     # print(sensor_forward_left.value())
+      return sensor_forward_left.value()
+
+if __name__ == "__main__":
+    forward_left_sensor()
