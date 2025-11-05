04db2cd3372646f933625ba6ab3a44de4c72e1f7
DROP DATABASE <112593979+iyhp2dabread@users.noreply.github.com>
2025-11-05 09:26:27 +0000

    Create tiny_code_red_led_switch.py

---
+++ ResetBot/tiny_code_red_led_switch.py
@@ -0,0 +1,21 @@
+from machine import Pin
+from utime import sleep
+
+def tiny_code_red_led_switch_on(): #turns QR Code reader and red LED on
+    #tiny code reader and red LED control pin
+    switch_pin = 8  # Pin 8 = GP8 (labelled 11 on the jumper)
+    switch = Pin(switch_pin, Pin.OUT)
+
+    print("QR Code Reader and Red LED ON")
+    switch.value(1)
+
+def tiny_code_red_led_switch_off(): #turns QR Code reader and red LED off
+    #tiny code reader and red LED control pin
+    switch_pin = 8  # Pin 8 = GP8 (labelled 11 on the jumper)
+    switch = Pin(switch_pin, Pin.OUT)
+
+    print("QR Code Reader and Red LED OFF")
+    switch.value(0)
+
+if __name__ == "__main__":
+    tiny_code_red_led_switch_off()
