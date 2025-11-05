98d560cbb11a0c01d66bf5b09cf673a4fe911e01
DROP DATABASE <112593979+iyhp2dabread@users.noreply.github.com>
2025-11-05 09:22:44 +0000

    Create left_line_sensor.py

---
+++ ResetBot/left_line_sensor.py
@@ -0,0 +1,18 @@
+#line sensor detection for robot
+from machine import Pin
+from utime import sleep
+
+#Set the line_sensor pin for left
+sensor_pin = 10 # Pin 10 = GP10 (labelled 14 on the jumper)
+sensor_left = Pin(sensor_pin, Pin.IN, Pin.PULL_DOWN)
+
+#Continiously update the sensor value and print said value
+def left_sensor():
+    #print("Sequence Left Active")
+    while True:
+      sleep(0.01)
+      print(sensor_left.value())
+      return sensor_left.value()
+
+if __name__ == "__main__":
+    left_sensor()
